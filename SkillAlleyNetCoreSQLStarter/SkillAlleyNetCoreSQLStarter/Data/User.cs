e28ad27bd8128c37c249fbe618d1ed30c1969916
Amit Chawla <acamit84@gmail.com>
2021-03-22 01:09:05 +0530

    Add models

---
+++ SkillAlleyNetCoreSQLStarter/SkillAlleyNetCoreSQLStarter/Data/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SkillAlleyNetCoreSQLStarter.Data
+{
+    public class User
+    {
+        [Key]
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+    }
+}
