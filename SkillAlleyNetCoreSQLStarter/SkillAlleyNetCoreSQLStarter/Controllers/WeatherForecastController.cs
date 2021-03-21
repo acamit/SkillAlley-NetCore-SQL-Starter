79dd8cf88e0f52ec9fcafea01ef9bce1268d4445
Amit Chawla <acamit84@gmail.com>
2021-03-22 02:19:05 +0530

    access the records from db

--- SkillAlleyNetCoreSQLStarter/SkillAlleyNetCoreSQLStarter/Controllers/WeatherForecastController.cs
+++ SkillAlleyNetCoreSQLStarter/SkillAlleyNetCoreSQLStarter/Controllers/WeatherForecastController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using SkillAlleyNetCoreSQLStarter.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +12,18 @@ namespace SkillAlleyNetCoreSQLStarter.Controllers
     public class WeatherForecastController : ControllerBase
     {
         private readonly ILogger<WeatherForecastController> _logger;
+        private readonly ApplicationDbContext _dbContext;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, ApplicationDbContext dbContext)
         {
             _logger = logger;
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult ShowAllRecords() {
+            var result = _dbContext.Weather.ToList();
+            return Ok(result);
         }
     }
 }
