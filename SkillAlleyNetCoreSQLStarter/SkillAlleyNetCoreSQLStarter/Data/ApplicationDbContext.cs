3be4bdf73cd30dd6b29b435fc354c4d3b878a18c
Amit Chawla <acamit84@gmail.com>
2021-03-22 00:08:27 +0530

    Add Application context class

---
+++ SkillAlleyNetCoreSQLStarter/SkillAlleyNetCoreSQLStarter/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SkillAlleyNetCoreSQLStarter.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){}
+    }
+}
