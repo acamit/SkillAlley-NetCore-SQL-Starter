6bd5d3b153ca47d5cbd7af43aab2c7d9c6cefea4
Amit Chawla <acamit84@gmail.com>
2021-03-22 01:09:05 +0530

    Add models

---
+++ SkillAlleyNetCoreSQLStarter/SkillAlleyNetCoreSQLStarter/Data/Weather.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SkillAlleyNetCoreSQLStarter.Data
+{
+    public class Weather
+    {
+        [Key]
+        public long Id { get; set; }
+        public DateTime Date { get; set; }
+        public double TemperatureC { get; set; }
+        public string Summary { get; set; }
+    }
+
+}
